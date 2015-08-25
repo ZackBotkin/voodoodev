2e35ef40d46cdb11a16c65c96b63371823fb7b53
Zack Botkin <zackbotkin@gmail.com>
2015-08-25 01:29:07 -0400

    im stuck :/

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,21 @@
 
-var module = angular.module('brainless', []);
+var app = angular.module('brainless', ['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                //templateUrl: 'partials/home.html'
+                template: '<div> test </div>'
+            });
+    }
+
+]);
+
+
