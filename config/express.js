09c8da908a3bce634d6a2fa5d75b0c486d479264
Zack Botkin <zackbotkin@gmail.com>
2015-08-24 23:53:09 -0400

    reworking the structure

--- config/express.js
+++ config/express.js
@@ -35,7 +35,7 @@ module.exports = function() {
     require('../app/routes/index.server.routes.js')(app);
     require('../app/routes/users.server.routes.js')(app);
 
-    // app.use(express.static('./public'));
+    app.use(express.static('./public'));
 
     return app;
 };
---
+++ public/js/controllers.js
@@ -0,0 +1,8 @@
+
+var module = angular.module('brainless', []);
+
+module.controller('MainCtrl', [
+    '$scope',
+    function($scope) {
+    }
+]);
